6168cd899e9461a4a0a84872191307bc539fe919
paulitics <paulodun08@gmail.com>
2020-03-24 22:05:05 +0100

    added start.js

---
+++ start.js
@@ -0,0 +1,19 @@
+let name = "Paul";
+name = "Paul"
+
+console.log(name);
+
+var courses = ["HTML", "CSS", "Javascript", "Design"];
+
+console.log(courses);
+
+for (let i=0; i<=200; i++) {
+
+   if (i%2===0) {
+
+     courses.push(i);
+
+   }
+
+}
+console.log(courses)
